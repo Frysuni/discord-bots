d9575d8e794016c3b3e2380b06eb44d30f6e51f5
Frysuni <ibragimzinatullin@mail.ru>
2022-07-11 17:03:33 +0500

    изменения

--- commands/ping.js
+++ commands/ping.js
@@ -6,6 +6,6 @@ module.exports = {
         .setDescription('Не, ну это легенда'),
     async ping(interaction) {
         const sent = await interaction.reply({ content: 'Замер пинга...', fetchReply: true, ephemeral: true });
-        interaction.editReply(`Пинг: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+        interaction.editReply(`Пинг: ${sent.createdTimestamp - interaction.createdTimestamp}мс`);
     }
 }
